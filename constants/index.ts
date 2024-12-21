6ad5201a85a0049995df948c868ebf6da264ce58
hellogellohello <145465184+hellogellohello@users.noreply.github.com>
2024-12-21 13:16:11 +0530

    Update index.ts

--- constants/index.ts
+++ constants/index.ts
@@ -4,22 +4,6 @@ export const sidebarLinks = [
     route: '/',
     label: 'Home',
   },
-
-  {
-    imgURL: '/icons/upcoming.svg',
-    route: '/upcoming',
-    label: 'Upcoming',
-  },
-  {
-    imgURL: '/icons/previous.svg',
-    route: '/previous',
-    label: 'Previous',
-  },
-  {
-    imgURL: '/icons/Video.svg',
-    route: '/recordings',
-    label: 'Recordings',
-  },
   {
     imgURL: '/icons/add-personal.svg',
     route: '/personal-room',
